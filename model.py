64d54127ac258d8920eadbe246cb611213ece2d4
angieMutava <mutavaangela@yahoo.com>
2016-11-22 22:52:40 -0800

    models classes

---
+++ model.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class SongFinder(Base):
+	__tablename__ = 'lyrics'
+	# Here we define columns for the table lyrics
+    # Each column is also a normal Python instance attribute.
+	id = Column(Integer, primary_key=True)
+	song_id = Column(String(200), nullable=False)
+	song_lyrics = Column(String(700), nullable=False)
+
+# Create an engine that stores data in the local directory's
+# songs.db file.
+engine = create_engine('sqlite:///songs.db', echo=True)
+
+# Create all tables in the engine. This is equivalent to "Create Table"
+# statements in raw SQL.
+Base.metadata.create_all(engine)
+
+
