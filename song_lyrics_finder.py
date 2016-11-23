f68ed5f42640049260aa367b8ea96e16294fde22
angieMutava <mutavaangela@yahoo.com>
2016-11-22 22:55:18 -0800

    lyrics finder functions

---
+++ song_lyrics_finder.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlachemy.orm import session_maker
+
+from model import SongFinder, Base
+
+engine = create_engine('sqlite:///songs.db', echo=True)
+
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
