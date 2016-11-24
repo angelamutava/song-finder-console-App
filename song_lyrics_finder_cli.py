f2eeb90327bd5feeac4b876ec74c48508ee89e76
angieMutava <mutavaangela@yahoo.com>
2016-11-24 10:47:23 -0800

    delete file

--- song_lyrics_finder_cli.py
+++
@@ -1,9 +0,0 @@
-import click
-from song_lyrics_finder import *
-
-@click.group()
-@click.command()
-@click.option('--name', prompt='Your name please.')
-
-def hello(name):
-	click.secho('Hello %s Welcome to lyrics finder' % name, fg = "green")
